37537db9d41547b0590b4fbffad4e6b122129669
albertogiunta <alb.giunta@gmail.com>
2017-08-14 13:44:10 +0200

    fix

---
+++ app/src/main/java/com/jaus/albertogiunta/teseo/screens/areaNavigation/AreaNavigationActivity.kt
@@ -0,0 +1,172 @@
+package com.jaus.albertogiunta.teseo
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.jaus.albertogiunta.teseo.data.*
+import com.jaus.albertogiunta.teseo.helpers.Direction
+import com.jaus.albertogiunta.teseo.helpers.SIGNAL_STRENGTH
+import com.jaus.albertogiunta.teseo.screens.BaseActivity
+import kotlinx.android.synthetic.main.layout_launch.*
+import kotlinx.android.synthetic.main.layout_normal_navigation.*
+import org.jetbrains.anko.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
+
+
+interface View : AreaUpdateListener, UserPositionListener, RouteListener, CellUpdateListener, SignalListener {
+
+    /**
+     * Get the view context
+     */
+    fun context(): Context
+
+}
+
+@Suppress("EXPERIMENTAL_FEATURE_WARNING")
+class MainActivity : View, BaseActivity() {
+
+    private lateinit var presenter: MainPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_area_navigation)
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1)
+        }
+
+        presenter = MainPresenter(this)
+
+        btnFirstConnect.setOnClickListener {
+            //            presenter.askConnection()
+//            layoutFirst.visibility = android.view.View.GONE
+//            layoutSecond.visibility = android.view.View.VISIBLE
+//            startActivityForResult(Intent(this, InitialSetupActivity::class.java), 1)
+        }
+
+        btnLaterConnect.setOnClickListener {
+            presenter.askConnection()
+//            startActivityForResult(Intent(this, InitialSetupActivity::class.java), 1)
+        }
+
+        btnRoute.setOnClickListener {
+            showRouteDialog()
+        }
+
+        btnUp.setOnClickListener {
+            detectedMovement(Direction.NORTH)
+        }
+        btnDown.setOnClickListener {
+            detectedMovement(Direction.SOUTH)
+        }
+        btnLeft.setOnClickListener {
+            detectedMovement(Direction.WEST)
+        }
+        btnRight.setOnClickListener {
+            detectedMovement(Direction.EAST)
+        }
+    }
+
+    override fun context(): Context {
+        return this@MainActivity
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        presenter.askConnection()
+    }
+
+    override fun onAreaUpdated(area: AreaViewedFromAUser) {
+        runOnUiThread {
+            drawView.drawArea()
+            btnRoute.visibility = android.view.View.VISIBLE
+        }
+    }
+
+    override fun onPositionChanged(userPosition: Point) {
+        runOnUiThread { drawView.drawUserPosition(userPosition) }
+    }
+
+    override fun onRouteReceived(route: List<RoomInfo>, isEmergency: Boolean) {
+        runOnUiThread {
+            drawView.drawRoute(route, isEmergency)
+            if (isEmergency) tvEmergencyMode.visibility = android.view.View.VISIBLE
+        }
+    }
+
+    override fun onRouteFollowedUntilEnd() {
+        runOnUiThread {
+            toast("You reached your destination!")
+            drawView.undrowRoute()
+        }
+    }
+
+    override fun onSignalStrengthUpdated(strength: SIGNAL_STRENGTH) {
+        tvSignalStrength.text = strength.text
+        tvSignalStrength.textColor = strength.tint
+    }
+
+    override fun onCellUpdated(cell: RoomViewedFromAUser) {
+        runOnUiThread { tvCurrentRoom.text = cell.info.id.name }
+    }
+
+    private fun detectedMovement(direction: Direction) {
+        vibrator.vibrate(20)
+        val maxStepsAtOnce = 10
+        for (i in 0 until maxStepsAtOnce) {
+            presenter.onMovementDetected(direction)
+        }
+    }
+
+    private fun showRouteDialog() {
+
+        val rooms = AreaState.area?.rooms?.map { (info) -> info.id.name }?.sorted()
+
+        var departure: String = ""
+        var arrival: String = ""
+
+        alert {
+            title = "Choose your route"
+            positiveButton("GO!") {
+                presenter.askRoute(departure, arrival)
+            }
+            customView {
+                linearLayout {
+                    orientation = LinearLayout.VERTICAL
+                    var tvDep: TextView? = null
+                    var tvArr: TextView? = null
+
+                    button("Choose departure") {
+                        onClick {
+                            selector("Choose the departure room", rooms!!, { _, i ->
+                                departure = rooms[i]
+                                tvDep?.text = "Departure:\t\t $departure"
+                            })
+                        }
+                    }
+                    tvDep = textView {
+                        padding = dip(16)
+                    }
+
+                    button("Choose arrival") {
+                        onClick {
+                            selector("Choose the arrival room", rooms!!, { _, i ->
+                                arrival = rooms[i]
+                                tvArr?.text = "Arrival:\t\t\t\t\t $arrival"
+                            })
+                        }
+                    }
+                    tvArr = textView {
+                        padding = dip(16)
+                    }
+                }
+            }
+        }.show()
+    }
+}
+
---
+++ app/src/main/java/com/jaus/albertogiunta/teseo/screens/areaNavigation/AreaNavigationPresenter.kt
@@ -0,0 +1,162 @@
+package com.jaus.albertogiunta.teseo
+
+import com.jaus.albertogiunta.teseo.data.*
+import com.jaus.albertogiunta.teseo.helpers.*
+import com.jaus.albertogiunta.teseo.networking.WSMessageCallbacks
+import com.jaus.albertogiunta.teseo.networking.WebSocketHelper
+import com.jaus.albertogiunta.teseo.util.IDExtractor
+import com.jaus.albertogiunta.teseo.util.Unmarshaler
+import trikita.log.Log
+
+class MainPresenter(val view: View) : AreaUpdateListener, UserMovementListener, UserPositionListener, SignalAndCellSwitchingListener, WSMessageCallbacks {
+
+    companion object {
+        const val FIRST_CONNECTION = "firstConnection"
+        const val NORMAL_CONNECTION = "normalConnection"
+        const val DISCONNECTION = "disconnect"
+        const val NORMAL_CONNECTION_RESPONSE = "ack"
+    }
+
+    var isSwitching: Boolean = false
+
+    var signal: SignalHelper = SignalHelper(this)
+
+    var webSocketHelper = WebSocketHelper(this)
+
+    var signalObservers: MutableList<SignalListener> = mutableListOf()
+    var positionObservers: MutableList<UserPositionListener> = mutableListOf()
+    var cellObservers: MutableList<CellUpdateListener> = mutableListOf()
+
+    var cell: RoomViewedFromAUser? = null
+        set(value) {
+            field = value
+            cellObservers.forEach { o ->
+                value?.let {
+                    Log.d(": $it")
+                    o.onCellUpdated(it)
+                }
+            }
+        }
+
+    var position: Point = Point(0, 0)
+        set(value) {
+            field = value
+            positionObservers.forEach { o -> o.onPositionChanged(value) }
+        }
+
+    var route: RouteResponseShort? = null
+        set(value) {
+            field = value
+            value?.let { view.onRouteReceived(IDExtractor.roomsInfoListFromIDs(it.route, AreaState.area!!), false) }
+        }
+
+    var emergencyRoute: RouteResponseShort? = null
+        set(value) {
+            field = value
+            value?.let { view.onRouteReceived(IDExtractor.roomsInfoListFromIDs(it.route, AreaState.area!!), true) }
+        }
+
+
+    init {
+        signalObservers.add(view)
+        cellObservers.add(view)
+        cellObservers.add(signal)
+        positionObservers.add(this)
+        positionObservers.add(signal)
+        positionObservers.add(view)
+    }
+
+    override fun onMovementDetected(direction: Direction) {
+        if (!isSwitching) {
+            val tempPosition = direction.operationOnPoint(position)
+            cell?.let {
+                if (MovementHelper.isMovementLegit(tempPosition, (cell as RoomViewedFromAUser).info.roomVertices, (cell as RoomViewedFromAUser).passages)) {
+                    position = tempPosition
+                } else {
+                    Log.d("onMovementDetected: movement was not legit for temp $tempPosition ||| position $position ||| direction $direction")
+                }
+            }
+        }
+    }
+
+    override fun onPositionChanged(userPosition: Point) {
+//        Log.d("onPositionChanged: " + route?.route?.last()?.serial + " " + cell?.info?.id?.serial)
+        if (isSetupFinished() && route?.route?.last()?.serial == cell?.info?.id?.serial) {
+            Log.d("onPositionChanged: invalidating route")
+            view.onRouteFollowedUntilEnd()
+            route = null
+        }
+    }
+
+    override fun onAreaUpdated(area: AreaViewedFromAUser) {
+        view.onAreaUpdated(area)
+        position = DistanceHelper.calulateMidPassage(area.rooms
+                .filter { (info) -> info.isEntryPoint }.first()
+                .passages.filter { p -> p.neighborId == MovementHelper.NEUTRAL_PASSAGE }.first())
+    }
+
+    override fun onConnectMessageReceived(connectMessage: String?) {
+        Log.d("onConnectMessageReceived: received message")
+        connectMessage?.let {
+            if (connectMessage == NORMAL_CONNECTION_RESPONSE && isSetupFinished()) {
+                cell = AreaState.area?.rooms?.filter { (infoCell) -> infoCell.id.serial == signal.bestNewCandidate.info.id.serial }?.first()
+                isSwitching = false
+                Log.d("onConnectMessageReceived: cellId" + cell?.info?.id)
+            } else if (connectMessage != NORMAL_CONNECTION_RESPONSE && !isSetupFinished()) {
+                onAreaUpdated(Unmarshaler.unmarshalArea(it))
+                cell = AreaState.area?.rooms?.filter({ (info) -> info.isEntryPoint })?.first()
+                Log.d("onConnectMessageReceived: cellId " + cell?.info?.id)
+            } else {
+                Log.d("onConnectMessageReceived: $connectMessage")
+            }
+        }
+    }
+
+    override fun onAlarmMessageReceived(alarmMessage: String?) {
+        Log.d("onAlarmMessageReceived: received ALARM")
+        alarmMessage?.let { emergencyRoute = Unmarshaler.unmarshalRouteResponse(it) }
+    }
+
+    override fun onRouteMessageReceived(routeMessage: String?) {
+        Log.d("onRouteMessageReceived: received ROUTE")
+        routeMessage?.let { route = Unmarshaler.unmarshalRouteResponse(it) }
+    }
+
+    override fun onSwitchToCellRequested(room: RoomViewedFromAUser) {
+        // use for test
+//        onConnectMessageReceived(NORMAL_CONNECTION_RESPONSE)
+        // use in production
+        isSwitching = true
+        webSocketHelper.handleSwitch(room.cell)
+    }
+
+    override fun onSignalStrengthUpdated(strength: SIGNAL_STRENGTH) {
+        signalObservers.forEach({ o -> o.onSignalStrengthUpdated(strength) })
+    }
+
+    /**
+     * Tells the appropriate websocket to connect, either for the first time or when the connection is lost
+     */
+    fun askConnection() {
+        // use for test
+//        if (area == null) onConnectMessageReceived(BufferedReader(InputStreamReader(view.context().resources.openRawResource(R.raw.area))).lines().collect(Collectors.joining("\n")))
+//        else onConnectMessageReceived(NORMAL_CONNECTION_RESPONSE)
+        // use in production
+        webSocketHelper.connectWS.send(if (AreaState.area == null) FIRST_CONNECTION else NORMAL_CONNECTION)
+    }
+
+    /**
+     * Contacts the websocket and tells it to ask for the specified route
+     */
+    fun askRoute(departureRoomName: String, arrivalRoomName: String) {
+        val depId: Int = AreaState.area!!.rooms.filter { (info) -> info.id.name == departureRoomName }.map { (info) -> info.id.serial }.first()
+        val arrId: Int = AreaState.area!!.rooms.filter { (info) -> info.id.name == arrivalRoomName }.map { (info) -> info.id.serial }.first()
+        Log.d("askRoute: uri$depId-uri$arrId")
+        AreaState.area?.let { webSocketHelper.routeWS.send("uri$depId-uri$arrId") }
+    }
+
+    private fun isSetupFinished(): Boolean {
+        return AreaState.area != null &&
+                cell != null
+    }
+}
